5de4031288f563930c96a8727b25818ea11f956b
Alexandra Bledea <bledea_alexandra@yahoo.com>
2025-01-07 02:14:38 +0200

    Fixed layout issues and added third component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HistoryComponentComponent } from './history-component/history-component.component';
+import { FeelingVsEmotionComponent } from './feeling-vs-emotion/feeling-vs-emotion.component';
+import { DefinitionsComponent } from './definitions/definitions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistoryComponentComponent,
+    FeelingVsEmotionComponent,
+    DefinitionsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feeling-vs-emotion/feeling-vs-emotion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feeling-vs-emotion',
+  templateUrl: './feeling-vs-emotion.component.html',
+  styleUrls: ['./feeling-vs-emotion.component.scss']
+})
+export class FeelingVsEmotionComponent {
+
+}
